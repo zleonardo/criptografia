4838fdbc8bbf078c6b37c57ca0cd9cda5265a9ba
zleo <leo_zacc8@hotmail.com>
2018-10-10 10:45:58 -0300

    first commit

---
+++ DES/des.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (){
+
+	//Leitura da entrada
+
+	//Monta tabela de entrada
+
+	//Divide bloco em dois
+
+	//Expande segunda metade para 48 bits
+
+	//Tratar chave
+
+	//XOR com o bloco de 48 bits
+
+	//Montar tabelas Ss
+
+	//Subistitution choice (usar as tabelas S para montar a saida com 32 bits)
+
+	//Swap
+	//?
+
+	//Inverse initial permutation
+	//?
+
+	//Printar apos cada round
+
+	return 1;
+}
