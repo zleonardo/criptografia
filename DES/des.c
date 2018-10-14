993922a9f023d77db88042b2ed0af47ea21e7041
MatheusAThomaz <matheus.thomaz@dcomp.sor.ufscar.br>
2018-10-14 15:55:07 -0300

    sbox implemented

--- DES/des.c
+++ DES/des.c
@@ -10,7 +10,58 @@ static int expansionTable[48] = {
     16, 17, 18, 19, 20, 21, 
     20, 21, 22, 23, 24, 25, 
     24, 25, 26, 27, 28, 29, 
-    28, 29, 30, 31, 32,  1};
+    28, 29, 30, 31, 32,  1
+}; 
+
+static char S[8][64] = {{
+    /* S1 */
+    14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,  
+     0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,  
+     4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0, 
+    15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
+},{
+    /* S2 */
+    15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,  
+     3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,  
+     0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15, 
+    13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9
+},{
+    /* S3 */
+    10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,  
+    13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,  
+    13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
+     1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12
+},{
+    /* S4 */
+     7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15,  
+    13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9,  
+    10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
+     3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14
+},{
+    /* S5 */
+     2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9, 
+    14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6, 
+     4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14, 
+    11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3
+},{
+    /* S6 */
+    12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
+    10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
+     9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
+     4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13
+},{
+    /* S7 */
+     4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
+    13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
+     1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
+     6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12
+},{
+    /* S8 */
+    13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
+     1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
+     7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
+     2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11
+}};
 
 
 unsigned char reverse(unsigned char b)
@@ -107,6 +158,84 @@ unsigned char *expansionPermutation(unsigned char table[4]) {
 	}
 
 	return expandedTable;
+}
+
+unsigned char *createSBox(unsigned char bytes[6]) {
+	unsigned char *sboxTable = malloc(sizeof(unsigned char)*8);
+	unsigned char aux = 0x00;
+
+	aux = bytes[0];
+	sboxTable[0] = aux >> 2;
+
+	aux = bytes[1];
+	sboxTable[1] = ((bytes[0] << 4) & 0x30) | (aux >> 4);
+	
+	aux = bytes[2];
+	sboxTable[2] = ((bytes[1] & 0x0f) << 2) | ((aux & 0xC0) >> 6);
+
+	sboxTable[3] = bytes[2] & 0x3f;
+
+	aux = bytes[3];
+	sboxTable[4] =  (aux >> 2);
+
+	aux = bytes[4];
+	sboxTable[5] = ((bytes[3] << 4) & 0x30) | (aux >> 4);
+
+	aux = bytes[5];
+	sboxTable[6] = ((bytes[4] & 0x0f) << 2) | ((aux & 0xC0) >> 6);
+
+	sboxTable[7] = bytes[5] & 0x3f;
+
+	return sboxTable;
+
+}
+
+unsigned char *substitutionChoice(unsigned char bytes[6]) {
+
+	unsigned char *sboxTable = malloc(sizeof(unsigned char)*4);
+
+	char byteCreated;
+
+	printf("\nbytes sbox\n");
+	for(int i = 0; i < 6; i++) {
+		printBinaryValue(bytes[i]);
+	}
+
+	unsigned char *sboxCreated = malloc(sizeof(unsigned char)*8);
+
+	sboxCreated = createSBox(bytes);
+
+	printf("\nbytes sbox\n");
+	for(int i = 0; i < 8; i++) {
+		printBinaryValue(sboxCreated[i]);
+	}
+
+
+	for(int i = 0; i < 8; i++) {
+
+		int row = (getAndMoveByte(2, 6, sboxCreated[i]) | getAndMoveByte(7, 7, sboxCreated[i]));
+
+		int column = (getAndMoveByte(3, 4, sboxCreated[i]) | getAndMoveByte(4, 5, sboxCreated[i])
+					| getAndMoveByte(5, 6, sboxCreated[i]) | getAndMoveByte(6, 7, sboxCreated[i]));
+
+		int index = (row  * 16) +  (column);
+		int sbox = i%8 + 1;
+		printf("\nrow %d \ncolumn:%d\n", row, column);
+
+		if(i % 2 == 0) {
+			printf("\ns %d\nvalue %d \nindex:%d\n", sbox, (S[i%8][index]), index);
+			byteCreated = (S[i%8][index] << 4);
+		} else {
+			printf("\ns %d\nvalue %d \nindex:%d\n", sbox, (S[i%8][index]), index);
+			byteCreated |= S[i%8][index];
+			sboxTable[i/2] = byteCreated;
+			printBinaryValue(byteCreated);
+			byteCreated = 0x00;
+		}
+
+	}
+
+	return sboxTable;
 }
 
 unsigned char *createTable(unsigned char plainText[8])
@@ -219,10 +348,16 @@ int main()
 	}
 	printf("\n\n");
 
-	//Montar tabelas Ss
-
 	//Subistitution choice (usar as tabelas S para montar a saida com 32 bits)
+	unsigned char *sboxTable;
 
+	sboxTable = substitutionChoice(expandedTable);
+
+	printf("\n\nsboxTable");
+	for(int i = 0; i < 4; i++) {
+		printf("\n%02x", sboxTable[i]);
+	}
+	printf("\n\n");	
 	//Swap
 	//?
 
